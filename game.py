7da5e7ea2a373f29598031d8c7f9555700a85b00
Marcin <wmartinezyo@gmail.com>
2017-11-19 00:56:03 +0100

    Implemented showing the game in graphics mode

--- game.py
+++ game.py
@@ -1,17 +1,20 @@
 import random 
+import tkinter as tk
 
 class GameOfLife:
-    def __init__(self, field_size):
-        self.X = self.Y = 10
+    def __init__(self, window, canvas, field_size):
+        self.X = self.Y = 80
         self.board = [[0,] * self.Y for _ in range (self.X)]
         self.board2 = [[0,] * self.Y for _ in range (self.X)]
+        self.canvas = canvas
+        self.window = window
         self.field_size = field_size
+        self.init_board(0.5)
 
-    def init_board(self, count):
-        for i in range (count):
-            x = random.randint(0, self.X-1)
-            y = random.randint(0, self.Y-1)
-            self.board[x][y] = 1
+    def init_board(self, threshold):
+        for x in range (self.X):
+            for y in range (self.Y):
+                self.board[x][y] = 1 if random.random() > 0.5 else 0
 
     def count_neighbours(self, x, y):
         xb, yb, xe, ye = max(x-1, 0), max(y-1, 0), min(x+1, self.X-1), min(y+1, self.Y-1)
@@ -29,18 +32,35 @@ class GameOfLife:
             for y in range(self.Y):
                 self.apply_rules(x, y)
 
-    def display(self):
+    def display_text(self):
         print()
         for line in self.board:
             print(" ".join(str(_) for _ in line))
+			
+    def display(self):
+        for x in range(self.X):
+            for y in range(self.Y):
+                color = 'black' if self.board[x][y] == 1 else 'white'
+                self.canvas.create_rectangle(x * self.field_size, y * self.field_size, x * self.field_size + self.field_size, y * self.field_size + self.field_size, fill = color)
+        self.window.update_idletasks()
     
-    def evolve(self, n):
-        self.init_board(20)
-        for it in range(n):
-            self.apply_rules_on_all()
-            self.board, self.board2 = self.board2, self.board
-            self.display()
+    def evolve(self):
+        self.apply_rules_on_all()
+        self.board, self.board2 = self.board2, self.board
+        self.display()
+        self.canvas.after(20, self.evolve)
 
-game = GameOfLife(10)
+def createCanvas():
+    window = tk.Tk()
+    window.title("Game of life")
+    canvas = tk.Canvas(window, width = 800, height = 800, bg = 'white')  
+    canvas.pack()                
+    return window, canvas
 
-game.evolve(5)
+window, canvas = createCanvas()
+
+game = GameOfLife(window, canvas, 10)
+
+game.evolve()
+
+window.mainloop()
